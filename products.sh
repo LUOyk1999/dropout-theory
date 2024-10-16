3abe0ad6638b312ae7cd80e218b3450f30d690f4
Yuankai Luo <yuankai0220@163.com>
2024-10-16 15:31:52 +0800

    init

--- products.sh
+++ products.sh
@@ -1,4 +1,5 @@
-python product.py --device 5 --ln --hidden_channels 200 --gnn gcn --dropout 0.5
-python product.py --device 5 --ln --hidden_channels 200 --gnn gcn --epochs 300 --dropout 0.0
-python product.py --device 5 --ln --hidden_channels 200 --gnn gcn --epochs 400 --num_layers 3
-python product.py --device 5 --ln --hidden_channels 200 --gnn gcn --epochs 400 --num_layers 3 --dropout 0.0
+python product.py --device 1 --ln --hidden_channels 200 --gnn gcn --epochs 400 --num_layers 3
+python product.py --device 1 --ln --hidden_channels 200 --gnn gcn --epochs 400 --num_layers 3 --dropout 0.0
+
+python product.py --device 5 --ln --hidden_channels 200 --gnn gcn
+python product.py --device 5 --ln --hidden_channels 200 --gnn gcn --dropout 0.0
